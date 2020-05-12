b79871252a7fcad5154307d8368f493c7b3cf2ee
jsonhyun <airplant@naver.com>
2020-05-12 21:58:16 +0900

    cafeVO 작성

---
+++ src/main/java/com/yi/domain/CafeVO.java
@@ -0,0 +1,327 @@
+package com.yi.domain;
+
+import java.util.Date;
+
+public class CafeVO {
+	private int cafeNo;
+	private String cafeName;
+	private ThemeVO themeNo;
+	private ZoneVO zoneNo;
+	private UsersVO userNo;
+	private String address;
+	private String detailAddress;
+	private String tel;
+	private String opentime;
+	private String closedDay;
+	private String sns;
+	private int tableNumber;
+	private int socketNumber;
+	private int toiletCdt;
+	private int mkdessertCdt;
+	private int terrasCdt;
+	private int reservationCdt;
+	private int wondooBuyCdt;
+	private int bookCdt;
+	private int beerCdt;
+	private int goodsCdt;
+	private int wifiCdt;
+	private int puppyCdt;
+	private int reserveokCdt;
+	private int parkingCdt;
+	private int nokidsCdt;
+	private Date registrationDate;
+	private Date updateDate;
+	private int viewNumber;
+	private String content;
+	private String oneline;
+	private int voteNumber;
+	private int powerlinkCdt;
+	private int cafeCdt;
+
+	public int getCafeNo() {
+		return cafeNo;
+	}
+
+	public void setCafeNo(int cafeNo) {
+		this.cafeNo = cafeNo;
+	}
+
+	public String getCafeName() {
+		return cafeName;
+	}
+
+	public void setCafeName(String cafeName) {
+		this.cafeName = cafeName;
+	}
+
+	public ThemeVO getThemeNo() {
+		return themeNo;
+	}
+
+	public void setThemeNo(ThemeVO themeNo) {
+		this.themeNo = themeNo;
+	}
+
+	public ZoneVO getZoneNo() {
+		return zoneNo;
+	}
+
+	public void setZoneNo(ZoneVO zoneNo) {
+		this.zoneNo = zoneNo;
+	}
+
+	public UsersVO getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(UsersVO userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDetailAddress() {
+		return detailAddress;
+	}
+
+	public void setDetailAddress(String detailAddress) {
+		this.detailAddress = detailAddress;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getOpentime() {
+		return opentime;
+	}
+
+	public void setOpentime(String opentime) {
+		this.opentime = opentime;
+	}
+
+	public String getClosedDay() {
+		return closedDay;
+	}
+
+	public void setClosedDay(String closedDay) {
+		this.closedDay = closedDay;
+	}
+
+	public String getSns() {
+		return sns;
+	}
+
+	public void setSns(String sns) {
+		this.sns = sns;
+	}
+
+	public int getTableNumber() {
+		return tableNumber;
+	}
+
+	public void setTableNumber(int tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+
+	public int getSocketNumber() {
+		return socketNumber;
+	}
+
+	public void setSocketNumber(int socketNumber) {
+		this.socketNumber = socketNumber;
+	}
+
+	public int getToiletCdt() {
+		return toiletCdt;
+	}
+
+	public void setToiletCdt(int toiletCdt) {
+		this.toiletCdt = toiletCdt;
+	}
+
+	public int getMkdessertCdt() {
+		return mkdessertCdt;
+	}
+
+	public void setMkdessertCdt(int mkdessertCdt) {
+		this.mkdessertCdt = mkdessertCdt;
+	}
+
+	public int getTerrasCdt() {
+		return terrasCdt;
+	}
+
+	public void setTerrasCdt(int terrasCdt) {
+		this.terrasCdt = terrasCdt;
+	}
+
+	public int getReservationCdt() {
+		return reservationCdt;
+	}
+
+	public void setReservationCdt(int reservationCdt) {
+		this.reservationCdt = reservationCdt;
+	}
+
+	public int getWondooBuyCdt() {
+		return wondooBuyCdt;
+	}
+
+	public void setWondooBuyCdt(int wondooBuyCdt) {
+		this.wondooBuyCdt = wondooBuyCdt;
+	}
+
+	public int getBookCdt() {
+		return bookCdt;
+	}
+
+	public void setBookCdt(int bookCdt) {
+		this.bookCdt = bookCdt;
+	}
+
+	public int getBeerCdt() {
+		return beerCdt;
+	}
+
+	public void setBeerCdt(int beerCdt) {
+		this.beerCdt = beerCdt;
+	}
+
+	public int getGoodsCdt() {
+		return goodsCdt;
+	}
+
+	public void setGoodsCdt(int goodsCdt) {
+		this.goodsCdt = goodsCdt;
+	}
+
+	public int getWifiCdt() {
+		return wifiCdt;
+	}
+
+	public void setWifiCdt(int wifiCdt) {
+		this.wifiCdt = wifiCdt;
+	}
+
+	public int getPuppyCdt() {
+		return puppyCdt;
+	}
+
+	public void setPuppyCdt(int puppyCdt) {
+		this.puppyCdt = puppyCdt;
+	}
+
+	public int getReserveokCdt() {
+		return reserveokCdt;
+	}
+
+	public void setReserveokCdt(int reserveokCdt) {
+		this.reserveokCdt = reserveokCdt;
+	}
+
+	public int getParkingCdt() {
+		return parkingCdt;
+	}
+
+	public void setParkingCdt(int parkingCdt) {
+		this.parkingCdt = parkingCdt;
+	}
+
+	public int getNokidsCdt() {
+		return nokidsCdt;
+	}
+
+	public void setNokidsCdt(int nokidsCdt) {
+		this.nokidsCdt = nokidsCdt;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public int getViewNumber() {
+		return viewNumber;
+	}
+
+	public void setViewNumber(int viewNumber) {
+		this.viewNumber = viewNumber;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getOneline() {
+		return oneline;
+	}
+
+	public void setOneline(String oneline) {
+		this.oneline = oneline;
+	}
+
+	public int getVoteNumber() {
+		return voteNumber;
+	}
+
+	public void setVoteNumber(int voteNumber) {
+		this.voteNumber = voteNumber;
+	}
+
+	public int getPowerlinkCdt() {
+		return powerlinkCdt;
+	}
+
+	public void setPowerlinkCdt(int powerlinkCdt) {
+		this.powerlinkCdt = powerlinkCdt;
+	}
+
+	public int getCafeCdt() {
+		return cafeCdt;
+	}
+
+	public void setCafeCdt(int cafeCdt) {
+		this.cafeCdt = cafeCdt;
+	}
+
+	@Override
+	public String toString() {
+		return "CafeVO [cafeNo=" + cafeNo + ", cafeName=" + cafeName + ", themeNo=" + themeNo + ", zoneNo=" + zoneNo
+				+ ", userNo=" + userNo + ", address=" + address + ", detailAddress=" + detailAddress + ", tel=" + tel
+				+ ", opentime=" + opentime + ", closedDay=" + closedDay + ", sns=" + sns + ", tableNumber="
+				+ tableNumber + ", socketNumber=" + socketNumber + ", toiletCdt=" + toiletCdt + ", mkdessertCdt="
+				+ mkdessertCdt + ", terrasCdt=" + terrasCdt + ", reservationCdt=" + reservationCdt + ", wondooBuyCdt="
+				+ wondooBuyCdt + ", bookCdt=" + bookCdt + ", beerCdt=" + beerCdt + ", goodsCdt=" + goodsCdt
+				+ ", wifiCdt=" + wifiCdt + ", puppyCdt=" + puppyCdt + ", reserveokCdt=" + reserveokCdt + ", parkingCdt="
+				+ parkingCdt + ", nokidsCdt=" + nokidsCdt + ", registrationDate=" + registrationDate + ", updateDate="
+				+ updateDate + ", viewNumber=" + viewNumber + ", content=" + content + ", oneline=" + oneline
+				+ ", voteNumber=" + voteNumber + ", powerlinkCdt=" + powerlinkCdt + ", cafeCdt=" + cafeCdt + "]";
+	}
+
+}
