507aec68afd61a4356ac0d8939cfad3c103b7fc6
jamesareuming <hothihi5@gmail.com>
2020-07-07 01:12:12 +0900

    포인트샵, 공지사항 틀 맞춤

--- src/main/java/com/yi/controller/UserNoticController.java
+++ src/main/java/com/yi/controller/UserNoticController.java
@@ -2,12 +2,26 @@ package com.yi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user/*")
 public class UserNoticController {
+	
 	/*이벤트*/
+	@RequestMapping(value = "/notice/event", method = RequestMethod.GET)
+	public String eventList() throws Exception{
+		return "/user/userNoticeEventList";
+	}	
 	/*FAQ*/
+	@RequestMapping(value = "/notice/faq", method = RequestMethod.GET)
+	public String faqList() throws Exception{
+		return "/user/userNoticeFaqList";
+	}	
 	/*QNA*/
+	@RequestMapping(value = "/notice/qna", method = RequestMethod.GET)
+	public String qnaList() throws Exception{
+		return "/user/userNoticeQnaList";
+	}	
 
 }
--- src/main/java/com/yi/controller/UserPointShopController.java
+++ src/main/java/com/yi/controller/UserPointShopController.java
@@ -2,14 +2,30 @@ package com.yi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user/*")
 public class UserPointShopController {
 	
 	/*MuKKa 상품권*/
+	@RequestMapping(value = "/pointShop/giftCard", method = RequestMethod.GET)
+	public String giftCardList() throws Exception{
+		return "/user/userPointShopGiftCardList";
+	}
 	/*커피 & 차*/
+	@RequestMapping(value = "/pointShop/coffeeNtea", method = RequestMethod.GET)
+	public String coffeeNteaList() throws Exception{
+		return "/user/userPointShopcoffeeNteaList";
+	}	
 	/*커피 용품*/
+	@RequestMapping(value = "/pointShop/coffeeGoods", method = RequestMethod.GET)
+	public String coffeeGoodsList() throws Exception{
+		return "/user/userPointShopcoffeeGoodsList";
+	}	
 	/*기타*/	
-
+	@RequestMapping(value = "/pointShop/shopEtc", method = RequestMethod.GET)
+	public String shopEtcList() throws Exception{
+		return "/user/userPointShopshopEtcList";
+	}
 }
